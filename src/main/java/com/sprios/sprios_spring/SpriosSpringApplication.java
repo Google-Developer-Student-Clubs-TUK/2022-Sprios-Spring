de2e0c3d935c087f8375cb531325fc3a57624f01
Identityless <rlandwnd555@naver.com>
2022-11-29 17:49:15 +0900

    init : project setup

---
+++ src/main/java/com/sprios/sprios_spring/SpriosSpringApplication.java
@@ -0,0 +1,13 @@
+package com.sprios.sprios_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpriosSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpriosSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sprios/sprios_spring/SpriosSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sprios.sprios_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpriosSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
