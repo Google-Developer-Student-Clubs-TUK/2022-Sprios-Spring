9002da2ebfa8f9fa21103e5680b288fd978709d2
JeongHunHui <deca3248@gmail.com>
2022-12-02 19:46:17 +0900

    feat: 게시물의 좋아요에대한 정보를 담고있는 엔티티 생성

---
+++ src/main/java/com/sprios/sprios_spring/domain/post/entity/PostLike.java
@@ -0,0 +1,34 @@
+package com.sprios.sprios_spring.domain.post.entity;
+
+import com.sprios.sprios_spring.domain.member.entity.Member;
+import com.sprios.sprios_spring.global.entity.BaseEntity;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class PostLike extends BaseEntity {
+  @Id
+  @GeneratedValue(strategy = GenerationType.SEQUENCE)
+  @Column(name = "post_like_id")
+  private Long id;
+
+  @ManyToOne
+  @JoinColumn(name = "post_id")
+  private Post post;
+
+  @ManyToOne
+  @JoinColumn(name = "member_id")
+  private Member member;
+
+  @Builder
+  private PostLike(Post post, Member member) {
+    this.post = post;
+    this.member = member;
+  }
+}
