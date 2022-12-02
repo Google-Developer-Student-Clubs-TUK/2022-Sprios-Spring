296bb145b0cb0aac3831d20951c59da74975903a
JeongHunHui <deca3248@gmail.com>
2022-12-02 18:24:39 +0900

    feat: 다른 엔티티의 부모 클래스가 되는 BaseEntity 추가

---
+++ src/main/java/com/sprios/sprios_spring/global/entity/BaseEntity.java
@@ -0,0 +1,34 @@
+package com.sprios.sprios_spring.global.entity;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.hibernate.annotations.DynamicInsert;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@DynamicInsert
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+@ToString
+public class BaseEntity {
+  @CreatedDate private LocalDateTime createdAt;
+
+  @LastModifiedDate private LocalDateTime updatedAt;
+
+  @Column(columnDefinition = "boolean default false")
+  private Boolean isDeleted;
+
+  public void deleteEntity() {
+    this.isDeleted = true;
+  }
+}
