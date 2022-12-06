623848b8212133f2e1288005ead203e4e6a5b9c4
JeongHunHui <deca3248@gmail.com>
2022-12-07 04:42:19 +0900

    feat: 회원 등록, 회원 중복 여부 확인, 로그인(아직 세션사용X) API를 담은 MemberController 구현

---
+++ src/main/java/com/sprios/sprios_spring/domain/member/controller/MemberController.java
@@ -0,0 +1,64 @@
+package com.sprios.sprios_spring.domain.member.controller;
+
+import com.sprios.sprios_spring.domain.member.dto.LoginRequest;
+import com.sprios.sprios_spring.domain.member.dto.MemberDto;
+import com.sprios.sprios_spring.domain.member.entity.Member;
+import com.sprios.sprios_spring.domain.member.exception.MemberDuplicatedException;
+import com.sprios.sprios_spring.domain.member.service.MemberService;
+import com.sprios.sprios_spring.global.result.ResultResponse;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+import static com.sprios.sprios_spring.domain.member.controller.MemberController.MEMBER_API_URI;
+import static com.sprios.sprios_spring.global.result.ResultCode.*;
+
+@RestController
+@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
+@Slf4j
+@RequestMapping(MEMBER_API_URI)
+public class MemberController {
+
+  public static final String MEMBER_API_URI = "/api/members";
+
+  private final MemberService memberService;
+
+  // private final LoginService loginService;
+  private final PasswordEncoder passwordEncoder;
+
+  @PostMapping
+  public ResponseEntity<ResultResponse> registration(@RequestBody @Valid MemberDto memberDto) {
+    if (memberService.isDuplicatedAccount(memberDto.getAccount())) {
+      throw new MemberDuplicatedException();
+    }
+    Member member = MemberDto.toEntity(memberDto, passwordEncoder);
+    memberService.registrationMember(member);
+    return ResponseEntity.ok(ResultResponse.of(MEMBER_REGISTRATION_SUCCESS));
+  }
+
+  @GetMapping("/duplicated/{account}")
+  public ResponseEntity<ResultResponse> isDuplicatedAccount(@PathVariable String account) {
+    boolean isDuplicated = memberService.isDuplicatedAccount(account);
+
+    if (isDuplicated) {
+      return ResponseEntity.badRequest().body(ResultResponse.of(MEMBER_ACCOUNT_DUPLICATED, true));
+    }
+    return ResponseEntity.ok(ResultResponse.of(MEMBER_ACCOUNT_NOT_DUPLICATED, false));
+  }
+
+  @PostMapping("/login")
+  public ResponseEntity<ResultResponse> login(@RequestBody @Valid LoginRequest loginRequest) {
+    boolean isValidMember = memberService.isValidMember(loginRequest, passwordEncoder);
+
+    if (isValidMember) {
+      // loginService.login(memberService.findMemberByAccount(memberDto.getAccount()).getId());
+      return ResponseEntity.ok(ResultResponse.of(MEMBER_LOGIN_SUCCESS, true));
+    }
+    return ResponseEntity.badRequest().body(ResultResponse.of(MEMBER_LOGIN_FAILURE, false));
+  }
+}
