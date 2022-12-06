0468e0b836197a2c909cfc6691e257e0e0e2401d
JeongHunHui <deca3248@gmail.com>
2022-12-06 15:43:27 +0900

    feat: MemberRepository에 메소드 추가

--- src/main/java/com/sprios/sprios_spring/domain/member/repository/MemberRepository.java
+++ src/main/java/com/sprios/sprios_spring/domain/member/repository/MemberRepository.java
@@ -3,5 +3,10 @@ package com.sprios.sprios_spring.domain.member.repository;
 import com.sprios.sprios_spring.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+  boolean existsByAccount(String account);
+
+  Optional<Member> findMemberByAccount(String account);
 }
