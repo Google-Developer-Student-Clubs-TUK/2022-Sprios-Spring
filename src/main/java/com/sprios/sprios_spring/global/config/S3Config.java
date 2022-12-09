4ee635d7e062e79d1e1b418ad17cd8e5af72b320
Identityless <rlandwnd555@naver.com>
2022-12-09 17:04:34 +0900

    fix : 오탈자 수정

--- src/main/java/com/sprios/sprios_spring/global/config/S3Config.java
+++ src/main/java/com/sprios/sprios_spring/global/config/S3Config.java
@@ -11,10 +11,10 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class S3Config {
-  @Value("$cloud.aws.credentials.access-key}")
+  @Value("${cloud.aws.credentials.accesskey}")
   private String accessKey;
 
-  @Value("${cloud.aws.credentials.secret-key}")
+  @Value("${cloud.aws.credentials.secretkey}")
   private String secretKey;
 
   @Value("${cloud.aws.region.static}")
