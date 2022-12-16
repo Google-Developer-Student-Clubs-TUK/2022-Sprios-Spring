d31eb289ecc052a66edbe6a5636ff3ab0e092385
Identityless <rlandwnd555@naver.com>
2022-12-17 05:57:11 +0900

    feat : 프로필 사진 업데이트 기능 구현

--- src/main/java/com/sprios/sprios_spring/domain/member/controller/MemberUpdateController.java
+++ src/main/java/com/sprios/sprios_spring/domain/member/controller/MemberUpdateController.java
@@ -1,14 +1,32 @@
 package com.sprios.sprios_spring.domain.member.controller;
 
+import com.sprios.sprios_spring.domain.member.service.LoginService;
+import com.sprios.sprios_spring.domain.member.service.MemberService;
+import com.sprios.sprios_spring.global.result.ResultCode;
+import com.sprios.sprios_spring.global.result.ResultResponse;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import static com.sprios.sprios_spring.domain.member.controller.MemberUpdateController.MEMBER_API_URI;
 
 @RestController
 @Slf4j
+@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
 @RequestMapping(MEMBER_API_URI)
 public class MemberUpdateController {
   public static final String MEMBER_API_URI = "/api/update";
+  private final LoginService loginService;
+
+  @PostMapping("/img")
+  public ResponseEntity<ResultResponse> uploadImage(@RequestParam MultipartFile uploadImage) {
+    loginService.uploadImgS3(uploadImage);
+    return ResponseEntity.ok(ResultResponse.of(ResultCode.MEMBER_IMAGEUPDATE_SUCCESS, true));
+  }
 }
