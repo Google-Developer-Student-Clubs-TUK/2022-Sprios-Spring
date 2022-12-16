52735fd5c003edfd43d4fc1773953ffce613cef3
Identityless <rlandwnd555@naver.com>
2022-12-17 05:53:03 +0900

    feat : Image 관련 편의 유틸 추가

---
+++ src/main/java/com/sprios/sprios_spring/global/util/ImageUtil.java
@@ -0,0 +1,53 @@
+package com.sprios.sprios_spring.global.util;
+
+import com.sprios.sprios_spring.domain.member.exception.NotSupportedImageTypeException;
+import com.sprios.sprios_spring.global.vo.Image;
+import com.sprios.sprios_spring.global.vo.ImageType;
+import org.springframework.web.multipart.MultipartFile;
+
+
+import java.text.Normalizer;
+import java.util.UUID;
+
+public class ImageUtil {
+  private static final int FILE_NAME = 0;
+  private static final int FIEL_TYPE = 1;
+  public static Image convertMultipartFiletoImage(MultipartFile file, String Url) {
+    final String originalName = Normalizer.normalize(file.getOriginalFilename(), Normalizer.Form.NFC);
+    System.out.println(originalName);
+    String[] fileInfo = originalName.split("\\.");
+    for(String a : fileInfo) {
+      System.out.println(a);
+    }
+
+    if(!isSupportedType(fileInfo[FIEL_TYPE])) {
+      throw new NotSupportedImageTypeException();
+    }
+
+    return Image.builder()
+        .imgName(fileInfo[FILE_NAME])
+        .imgType(ImageType.valueOf(fileInfo[FIEL_TYPE].toUpperCase()))
+        .imgUUID(UUID.randomUUID().toString())
+        .imgUrl(Url).build();
+  }
+
+  private static boolean isSupportedType(String type) {
+    ImageType[] imageTypes = ImageType.values();
+
+    for(ImageType imageType : imageTypes) {
+      if(imageType.toString().equals(type.toUpperCase())) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  public static Image getDefaultImg() {
+    return Image.builder()
+        .imgName("base_profile")
+        .imgType(ImageType.PNG)
+        .imgUrl("https://identitylessimgserver.s3.ap-northeast-2.amazonaws.com/member/base_profile.png")
+        .imgUUID("base-UUID")
+        .build();
+  }
+}
