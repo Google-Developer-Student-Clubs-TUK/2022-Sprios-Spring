cbd3ac356ab91d3d4acef740f61d41b450597325
JeongHunHui <deca3248@gmail.com>
2022-12-21 01:33:38 +0900

    feat: PostImageRepository 생성

---
+++ src/main/java/com/sprios/sprios_spring/domain/post/repository/PostImageRepository.java
@@ -0,0 +1,6 @@
+package com.sprios.sprios_spring.domain.post.repository;
+
+import com.sprios.sprios_spring.domain.post.entity.PostImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostImageRepository extends JpaRepository<PostImage, Long> {}
