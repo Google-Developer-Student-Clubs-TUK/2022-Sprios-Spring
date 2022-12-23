28b9c4617a7da1aa93695383918ee754fc4b6687
Identityless <rlandwnd555@naver.com>
2022-12-24 02:35:27 +0900

    fix : 멤버 이미지 변경 api 삭제, 멤버 정보 업데이트 api 추가

--- src/main/java/com/sprios/sprios_spring/domain/member/controller/MemberUpdateController.java
+++ src/main/java/com/sprios/sprios_spring/domain/member/controller/MemberUpdateController.java
@@ -1,5 +1,6 @@
 package com.sprios.sprios_spring.domain.member.controller;
 
+import com.sprios.sprios_spring.domain.member.dto.MemberUpdateProfileRequest;
 import com.sprios.sprios_spring.domain.member.service.MemberUpdateService;
 import com.sprios.sprios_spring.global.result.ResultCode;
 import com.sprios.sprios_spring.global.result.ResultResponse;
@@ -7,10 +8,7 @@ import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import static com.sprios.sprios_spring.domain.member.controller.MemberUpdateController.MEMBER_API_URI;
@@ -23,9 +21,10 @@ public class MemberUpdateController {
   public static final String MEMBER_API_URI = "/api/member/update";
   private final MemberUpdateService memberUpdateService;
 
-  @PostMapping("/img")
-  public ResponseEntity<ResultResponse> uploadImage(@RequestParam MultipartFile uploadImage) {
-    memberUpdateService.uploadImgS3(uploadImage);
+  @PostMapping
+  public ResponseEntity<ResultResponse> uploadImage(@RequestParam MultipartFile uploadImage,
+                                                    @RequestBody MemberUpdateProfileRequest memberUpdateProfileRequest) {
+    memberUpdateService.updateMemberProfile(uploadImage, memberUpdateProfileRequest);
     return ResponseEntity.ok(ResultResponse.of(ResultCode.MEMBER_IMAGEUPDATE_SUCCESS, true));
   }
 }
